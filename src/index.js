ee83311967ebec6e0e3429f5d34b632abeee1e03
Abhi-Rupnawar <132432996+Abhi-Rupnawar@users.noreply.github.com>
2024-11-18 23:30:52 +0530

    Basic code added

--- src/index.js
+++ src/index.js
@@ -2,4 +2,25 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<h1>Munch </h1>)
+const Chocolate = (props)=>{
+    console.log(props)
+    
+const {name,emoji}=props //destructuring
+  return(<> 
+    <h1>{props.name} - {props.emoji}</h1>
+        
+    </>)
+}
+
+// const Snacks = ()=>{
+//     return <h1>Snacks </h1>
+// }
+root.render(<>
+    <h1>All Chocolates 😍</h1>
+    
+    <Chocolate name="dairy milk" emoji="🧂"/>
+    <Chocolate name="kisme" emoji="😘"/>
+    <Chocolate name="kitkate" emoji="😍"/>
+    <Chocolate/>
+    {/* <Snacks/> */}
+</>)
